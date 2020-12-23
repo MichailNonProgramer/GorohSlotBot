88975373db053437149e3fa587104c8baa661dbd
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2020-12-23 21:01:25 +0500

    Bonus

--- src/main/java/com/urfu/GorohSlot/bonus/TimerBonus.java
+++ src/main/java/com/urfu/GorohSlot/bonus/TimerBonus.java
@@ -1,20 +1,20 @@
-package com.urfu.GorohSlot.bonus;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class TimerBonus {
-
-    public static void start(){
-        TimerTask repeatedTask = new TimerTask() {
-            public void run() {
-                Bonus.giveBonus();
-            }
-        };
-        Timer timer = new Timer("Timer");
-
-        long delay  = 1000L;
-        long period = 1000L;
-        timer.scheduleAtFixedRate(repeatedTask, delay, period);
-    }
-}
+//package com.urfu.GorohSlot.bonus;
+//
+//import java.util.Timer;
+//import java.util.TimerTask;
+//
+//public class TimerBonus {
+//
+//    public static void start(){
+//        TimerTask repeatedTask = new TimerTask() {
+//            public void run() {
+//                Bonus.giveBonus();
+//            }
+//        };
+//        Timer timer = new Timer("Timer");
+//
+//        long delay  = 1000L;
+//        long period = 1000L;
+//        timer.scheduleAtFixedRate(repeatedTask, delay, period);
+//    }
+//}
