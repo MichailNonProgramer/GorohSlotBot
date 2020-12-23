6ae321b0c9b807555a61d71435ecdb83d64a0175
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2020-12-23 21:03:07 +0500

    Bonus off

--- src/main/java/com/urfu/GorohSlot/Main.java
+++ src/main/java/com/urfu/GorohSlot/Main.java
@@ -1,6 +1,5 @@
 package com.urfu.GorohSlot;
 
-import com.urfu.GorohSlot.bonus.TimerBonus;
 import com.urfu.GorohSlot.bot.Bot;
 import com.urfu.GorohSlot.database.SQLHandler;
 import org.telegram.telegrambots.ApiContextInitializer;
